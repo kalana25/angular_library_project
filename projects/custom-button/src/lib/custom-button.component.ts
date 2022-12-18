30a84c9c959321d5b1dcaa023f0a5cfa5e6fb07b
Kalana Sachith <kal25mahaarachchi@gmail.com>
2022-12-18 13:59:12 +0530

    first custom button

--- projects/custom-button/src/lib/custom-button.component.ts
+++ projects/custom-button/src/lib/custom-button.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'lib-custom-button',
   template: `
-    <p>
-      custom-button works!
-    </p>
+  <button [style.color]="color">{{body}}</button>
   `,
   styles: [
   ]
 })
 export class CustomButtonComponent implements OnInit {
-
+  @Input() color:string = '#000';
+  @Input() body:string = 'Hello world';
   constructor() { }
 
   ngOnInit(): void {
